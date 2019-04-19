474807025246eca5b19af18d132053b8ea0b8e14
QingRang <970753519@qq.com>
2019-04-19 10:34:44 +0800

    动画状态机
    动画状态机

---
+++ Animation/Assets/Scripts/Biker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biker : MonoBehaviour {
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        float v = Input.GetAxisRaw("Vertical");
+        anim.SetFloat("Vertical", v);
+        //transform.Translate(Vector3.forward * v * Time.deltaTime * 4);
+	}
+}
